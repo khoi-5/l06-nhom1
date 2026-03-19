9d33f8a54f9225fbb9f432d4b6f39cbb343bac26
khoi-5 <tranminhkhoichaua5@gmail.com>
2026-03-19 18:48:11 +0700

    new

--- YoloUNO_PlatformIO-RTOS_Project/src/global.cpp
+++ YoloUNO_PlatformIO-RTOS_Project/src/global.cpp
@@ -11,8 +11,10 @@ const int   mqttPort     = 1883;
 
 String ssid = "ESP32-nhom1-l06!!!";
 String password = "12345678";
-String wifi_ssid = "ACLAB";
-String wifi_password= "ACLAB2023";
+// String wifi_ssid = "ACLAB";
+// String wifi_password= "ACLAB2023";
+String wifi_ssid = "BACH YEN";
+String wifi_password= "TU0918289939";
 boolean isWifiConnected = false;
 SemaphoreHandle_t xBinarySemaphoreInternet = xSemaphoreCreateBinary();
 
